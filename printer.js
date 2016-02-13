c69a14e9fee2f3f4ab7dfb0a675565637ca9b695
Marco Piraccini <marco.piraccini@gmail.com>
2016-02-13 10:02:07 +0100

    Changed dir structure

--- printer.js
+++ printer.js
@@ -1,1 +1,5 @@
-module.exports = require('./lib/printer');
+var binary = require('node-pre-gyp');
+var path = require('path');
+var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
+
+var printer = exports = module.exports = require(binding_path);

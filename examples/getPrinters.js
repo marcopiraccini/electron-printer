f605b5bd2b89f683596ebab66e63cb7e7b8bff24
Marco Piraccini <marco.piraccini@gmail.com>
2016-02-15 07:21:50 +0100

    Fixed path

--- examples/getPrinters.js
+++ examples/getPrinters.js
@@ -1,3 +1,3 @@
-var printer = require("../lib"),
+var printer = require("../printer"),
     util = require('util');
 console.log("installed printers:\n"+util.inspect(printer.getPrinters(), {colors:true, depth:10}));
